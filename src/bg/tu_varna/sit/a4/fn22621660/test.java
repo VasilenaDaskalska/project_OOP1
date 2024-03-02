383be99e668374c4f99bf7a77fc59ba4c402a59e
vasilena daskalska <vasilena.daskalska@petroffsoft.bg>
2024-03-02 10:40:32 +0200

    added base services and test validation

--- src/bg/tu_varna/sit/a4/fn22621660/test.java
+++ src/bg/tu_varna/sit/a4/fn22621660/test.java
@@ -1,4 +1,10 @@
 package bg.tu_varna.sit.a4.fn22621660;
-
+import bg.tu_varna.sit.a4.fn22621660.services.BaseService;
+import java.io.IOException;
 public class test {
+    public static void main(String[] args) throws IOException {
+        String fileName = "appsettings.Development.json";
+        BaseService baseService = new BaseService();
+        baseService.OpenFile(fileName);
+    }
 }
